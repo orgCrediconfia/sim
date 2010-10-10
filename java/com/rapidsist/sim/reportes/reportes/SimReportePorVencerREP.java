f849778f19f1080a6421eb58652d5e8bf045ba47
Christian <chris.garcia@rapidsist.com>
2010-10-10 14:01:18 -0500

    Se terminaron estos dos reportes, se modifico la REP, jrxml y la jsp

--- java/com/rapidsist/sim/reportes/reportes/SimReportePorVencerREP.java
+++ java/com/rapidsist/sim/reportes/reportes/SimReportePorVencerREP.java
@@ -42,7 +42,7 @@ public class SimReportePorVencerREP implements ReporteControlIN {
 		//String sCveEmpresa = request.getParameter("CveEmpresa");
 		String sClave = request.getParameter("CvePrestamoGpo");
 		
-		String sSql = 	"SELECT \n"+
+		String sSql = 	"SELECT\n"+
 							"FECHA_REPORTE,\n"+
 							"ID_REGIONAL,\n"+
 							"NOM_REGIONAL,\n"+
@@ -54,14 +54,25 @@ public class SimReportePorVencerREP implements ReporteControlIN {
 							"NUM_INTEGRANTES,\n"+
 							"CATEGORIA,\n"+
 							"NOM_GRUPO,\n"+
+							"MONTO_PRESTADO,\n"+
 							"DIAS_ANTIGUEDAD,\n"+
-							"FECHA_PROX_PAGO,\n"+
-							"FECHA_ULTIMO_MOV,\n"+
-							"FECHA_FIN_CICLO,\n"+
-							"COORDINADOR_GRUPO,\n"+
-							"TELEFONO_COORDINADOR\n"+
-							"FROM V_DATOS_REPORTES\n"+
-							"WHERE CVE_PRESTAMO = '" + (String)request.getParameter("CvePrestamoGpo") + "'\n";
+							"F_ULT_AMORTIZACION,\n"+
+							"SALDO_LIQUIDADO_CUOTA,\n"+
+							"CAPITAL_VENCIDO,\n"+
+							"INTERESES_VENCIDOS,\n"+
+							"SEGURO_VENCIDO,\n"+
+							"RECARGOS_VENCIDOS,\n"+
+							"TOTAL_SALDO_VENCIDO,\n"+
+							"INSOLUTO,\n"+
+							"INTERESES,\n"+
+							"SEGURO,\n"+
+							"TOTAL_SALDO_LIQUIDAR\n"+
+							"FROM\n"+
+							"V_REPORTE_CREDITOS_POR_VENCER\n";
+							
+							if (!request.getParameter("CvePrestamoGpo").equals("")) {
+								sSql = sSql + " AND UPPER(CVE_PRESTAMO) LIKE '%" + ((String) request.getParameter("CvePrestamoGpo")).toUpperCase()  + "%' \n";
+							}
 							
 							 System.out.println("*****************Paso por aqui****************:"+sSql);
 		
