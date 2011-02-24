30d1debcd31bd0781bbc78762d50b375de8b00bb
Miguel Rugerio <mrugerio@gmail.com>
2011-02-24 17:38:53 -0600

    Se desactivo el doble submit

--- java/com/rapidsist/sim/prestamo/cliente/SimPrestamoLineaFondeoCON.java
+++ java/com/rapidsist/sim/prestamo/cliente/SimPrestamoLineaFondeoCON.java
@@ -1,5 +1,5 @@
 /**
- * Sistema de administración de portales.
+ * Sistema de administraciï¿½n de portales.
  *
  * Copyright (c) 2008 Rapidisist S.A de C.V. Todos los derechos reservados
  */
@@ -21,33 +21,33 @@ import java.rmi.RemoteException;
 import java.util.Enumeration;
 
 /**
- * Esta clase se encarga de administrar los servicios de operación (alta, baja,
- * modificación y consulta) de los integrantes de un comité. Esta clase es llamada por
+ * Esta clase se encarga de administrar los servicios de operaciï¿½n (alta, baja,
+ * modificaciï¿½n y consulta) de los integrantes de un comitï¿½. Esta clase es llamada por
  * el servlet {@link CatalogoS CatalogoS}.
  */
 public class SimPrestamoLineaFondeoCON implements CatalogoControlConsultaIN, CatalogoControlActualizaIN{
 
 	/**
-	 * Ejecuta los servicios de consulta del catálogo.
-	 * @param parametros Parámetros que se recogen de la sesion del usuario y se le envian a la clase CON.
-	 * Estos parámetros son: CVE_GPO_EMPRESA (Clave del grupo empresa), Filtro (el valor
-	 * del filtro que se debe aplicar solo si se ejecuto el componente de catálogos con
+	 * Ejecuta los servicios de consulta del catï¿½logo.
+	 * @param parametros Parï¿½metros que se recogen de la sesion del usuario y se le envian a la clase CON.
+	 * Estos parï¿½metros son: CVE_GPO_EMPRESA (Clave del grupo empresa), Filtro (el valor
+	 * del filtro que se debe aplicar solo si se ejecuto el componente de catï¿½logos con
 	 * OperacionCatalogo=CT)
-	 * @param request Objeto que provee de información al servlet sobre el request del cliente. El
-	 * contenedor de servlets crea un objeto HttpServletRequest y lo envía como un parámetro a este método.
-	 * @param response Objeto que provee de información del servlet sobre el response del cliente. El
-	 * contenedor de servlets crea un objeto HttpServletResponse y lo envía como un parámetro a este método.
-	 * @param config Objeto que provee de información del servlet sobre el ServletConfig del cliente. El
-	 * contenedor de servlets crea un objeto ServletConfig y lo envía como un parámetro a este método.
+	 * @param request Objeto que provee de informaciï¿½n al servlet sobre el request del cliente. El
+	 * contenedor de servlets crea un objeto HttpServletRequest y lo envï¿½a como un parï¿½metro a este mï¿½todo.
+	 * @param response Objeto que provee de informaciï¿½n del servlet sobre el response del cliente. El
+	 * contenedor de servlets crea un objeto HttpServletResponse y lo envï¿½a como un parï¿½metro a este mï¿½todo.
+	 * @param config Objeto que provee de informaciï¿½n del servlet sobre el ServletConfig del cliente. El
+	 * contenedor de servlets crea un objeto ServletConfig y lo envï¿½a como un parï¿½metro a este mï¿½todo.
 	 * @param catalogoSL Instancia del Ejb CatalogoSL que ejecuta en la base de datos las
 	 * operaciones especificadas en la clase CON
-	 * @param contexto Objeto que contiene información acerca del entorno del servidor de
+	 * @param contexto Objeto que contiene informaciï¿½n acerca del entorno del servidor de
 	 * aplicaciones.
-	 * @param iTipoOperacion Operación que debe ejecutar la clase CON. Las operaciones se encuentran
+	 * @param iTipoOperacion Operaciï¿½n que debe ejecutar la clase CON. Las operaciones se encuentran
 	 * especificadas en la clase {@link com.rapidsist.portal.cliente.CatalogoControl CatalogoControl}
-	 * @return Resultado de la consulta y la página a donde se redirecciona el control.
-	 * @throws RemoteException Si se generó un error en el Ejb CatalogoSL.
-	 * @throws java.lang.Exception Si se generó un error dentro de la clase CON.
+	 * @return Resultado de la consulta y la pï¿½gina a donde se redirecciona el control.
+	 * @throws RemoteException Si se generï¿½ un error en el Ejb CatalogoSL.
+	 * @throws java.lang.Exception Si se generï¿½ un error dentro de la clase CON.
 	 */
 	public RegistroControl consulta(Registro parametros, HttpServletRequest request, HttpServletResponse response, ServletConfig config, CatalogoSL catalogoSL, Context contexto, int iTipoOperacion)throws RemoteException, Exception{
 		RegistroControl registroControl = new RegistroControl();
@@ -89,28 +89,28 @@ public class SimPrestamoLineaFondeoCON implements CatalogoControlConsultaIN, Cat
 	}
 
 	/**
-	 * Valida los párametros entrada y ejecuta los servicios de alta, baja o cambio.
+	 * Valida los pï¿½rametros entrada y ejecuta los servicios de alta, baja o cambio.
 	 * @param registro Parametros que se recogen de la sesion del usuario y se le envian a la clase CON.
-	 * Estos parámetros son: CVE_GPO_EMPRESA (Clave del grupo empresa), CVE_USUARIO_BITACORA (clave
+	 * Estos parï¿½metros son: CVE_GPO_EMPRESA (Clave del grupo empresa), CVE_USUARIO_BITACORA (clave
 	 * del usuario que realiza la operacion), RegistroOriginal (registro leido originalmente y
-	 * se utiliza cuando se ejecuta la operación de modificacion y se verifica que no se hallan
+	 * se utiliza cuando se ejecuta la operaciï¿½n de modificacion y se verifica que no se hallan
 	 * realizado modificaciones al registro).
-	 * @param request Objeto que provee de información al servlet sobre el request del cliente. El
-	 * contenedor de servlets crea un objeto HttpServletRequest y lo envía como un parámetro a este método.
-	 * @param response Objeto que provee de información del servlet sobre el response del cliente. El
-	 * contenedor de servlets crea un objeto HttpServletResponse y lo envía como un parámetro a este método.
-	 * @param config Objeto que provee de información del servlet sobre el ServletConfig del cliente. El
-	 * contenedor de servlets crea un objeto ServletConfig y lo envía como un parámetro a este método.
+	 * @param request Objeto que provee de informaciï¿½n al servlet sobre el request del cliente. El
+	 * contenedor de servlets crea un objeto HttpServletRequest y lo envï¿½a como un parï¿½metro a este mï¿½todo.
+	 * @param response Objeto que provee de informaciï¿½n del servlet sobre el response del cliente. El
+	 * contenedor de servlets crea un objeto HttpServletResponse y lo envï¿½a como un parï¿½metro a este mï¿½todo.
+	 * @param config Objeto que provee de informaciï¿½n del servlet sobre el ServletConfig del cliente. El
+	 * contenedor de servlets crea un objeto ServletConfig y lo envï¿½a como un parï¿½metro a este mï¿½todo.
 	 * @param catalogoSL Instancia del Ejb CatalogoSL que ejecuta en la base de datos las
 	 * operaciones especificadas en la clase CON
-	 * @param contexto Objeto que contiene información acerca del entorno del servidor de
+	 * @param contexto Objeto que contiene informaciï¿½n acerca del entorno del servidor de
 	 * aplicaciones.
-	 * @param iTipoOperacion Operación que debe ejecutar la clase CON. Las operaciones se encuentran
+	 * @param iTipoOperacion Operaciï¿½n que debe ejecutar la clase CON. Las operaciones se encuentran
 	 * especificadas en la clase {@link com.rapidsist.portal.cliente.CatalogoControl CatalogoControl}
-	 * @return Respuesta del servicio de alta, baja o cambio y la página a donde
+	 * @return Respuesta del servicio de alta, baja o cambio y la pï¿½gina a donde
 	 * se redirecciona el control.
-	 * @throws RemoteException Si se generó un error en el Ejb CatalogoSL.
-	 * @throws java.lang.Exception Si se generó un error dentro de la clase CON.
+	 * @throws RemoteException Si se generï¿½ un error en el Ejb CatalogoSL.
+	 * @throws java.lang.Exception Si se generï¿½ un error dentro de la clase CON.
 	 */
 	public RegistroControl actualiza(Registro registro, HttpServletRequest request, HttpServletResponse response, ServletConfig config, CatalogoSL catalogoSL, Context contexto, int iTipoOperacion)throws RemoteException, Exception{
 		RegistroControl registroControl = new RegistroControl();
@@ -122,6 +122,7 @@ public class SimPrestamoLineaFondeoCON implements CatalogoControlConsultaIN, Cat
 		registro.addDefCampo("DAO_APLICA_A",sAplica);
 		registro.addDefCampo("DAO_ID_PRESTAMO",sPrestamo);
 		registro.addDefCampo("DAO_ID_LINEA",sLinea);
+		registro.addDefCampo("DobleSubmit","Desabilitado");
 		
 		registroControl.resultadoCatalogo = catalogoSL.modificacion("SimPrestamoLineaFondeo", registro, 1);
 		
