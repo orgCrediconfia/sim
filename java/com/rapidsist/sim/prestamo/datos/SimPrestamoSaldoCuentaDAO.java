1ad2453a9500d8c67e6367fa2508be3b2b77b1a1
Minerva Salinas <msalinas@rapidsist.com>
2012-02-23 11:47:19 -0600

    Se modifico el error al consultar el saldo de la cuenta del cliente y se modificaron leyendas en reportes

--- java/com/rapidsist/sim/prestamo/datos/SimPrestamoSaldoCuentaDAO.java
+++ java/com/rapidsist/sim/prestamo/datos/SimPrestamoSaldoCuentaDAO.java
@@ -9,7 +9,6 @@ package com.rapidsist.sim.prestamo.datos;
 import com.rapidsist.comun.bd.Conexion2;
 import com.rapidsist.comun.bd.Registro;
 import com.rapidsist.portal.catalogos.OperacionConsultaRegistro;
-import java.util.LinkedList;
 import java.sql.SQLException;
 
 /**
@@ -29,28 +28,37 @@ public class SimPrestamoSaldoCuentaDAO extends Conexion2 implements OperacionCon
 		
 		if (parametros.getDefCampo("CONSULTA").equals("SALDO_INDIVIDUAL")) {
 			
-			sSql =  "SELECT \n"+
-					"P.ID_PRESTAMO, \n"+
-					"TO_CHAR(A.SDO_EFECTIVO,'999,999,999.99') SALDO_CUENTA, \n"+
-					"NVL(A.SDO_EFECTIVO,0) SDO_EFECTIVO, \n"+
-					"A.F_FOTO FECHA \n"+
-					"FROM \n"+
-					"PFIN_SALDO A, \n"+
-					"SIM_PRESTAMO P \n"+
-					"WHERE P.CVE_GPO_EMPRESA = '" + (String)parametros.getDefCampo("CVE_GPO_EMPRESA") + "' \n"+
-				    "AND P.CVE_EMPRESA = '" + (String)parametros.getDefCampo("CVE_EMPRESA") + "' \n"+
-				    "AND P.ID_PRESTAMO = '" + (String)parametros.getDefCampo("ID_PRESTAMO") + "' \n"+
-					"AND A.CVE_GPO_EMPRESA  = P.CVE_GPO_EMPRESA \n"+
-					"AND A.CVE_EMPRESA = P.CVE_EMPRESA \n"+
-					"AND A.ID_CUENTA = P.ID_CUENTA_REFERENCIA \n"+
-					"AND A.CVE_DIVISA = 'MXP' \n"+
-					"ORDER BY FECHA DESC \n";
+			sSql =  "SELECT * FROM PFIN_SALDO \n";
+			ejecutaSql();
+			if (rs.next()){
+				sSql =  "SELECT \n"+
+						"P.ID_PRESTAMO, \n"+
+						"TO_CHAR(A.SDO_EFECTIVO,'999,999,999.99') SALDO_CUENTA, \n"+
+						"NVL(A.SDO_EFECTIVO,0) SDO_EFECTIVO, \n"+
+						"A.F_FOTO FECHA \n"+
+						"FROM \n"+
+						"PFIN_SALDO A, \n"+
+						"SIM_PRESTAMO P \n"+
+						"WHERE P.CVE_GPO_EMPRESA = '" + (String)parametros.getDefCampo("CVE_GPO_EMPRESA") + "' \n"+
+					    "AND P.CVE_EMPRESA = '" + (String)parametros.getDefCampo("CVE_EMPRESA") + "' \n"+
+					    "AND P.ID_PRESTAMO = '" + (String)parametros.getDefCampo("ID_PRESTAMO") + "' \n"+
+						"AND A.CVE_GPO_EMPRESA  = P.CVE_GPO_EMPRESA \n"+
+						"AND A.CVE_EMPRESA = P.CVE_EMPRESA \n"+
+						"AND A.ID_CUENTA = P.ID_CUENTA_REFERENCIA \n"+
+						"AND A.CVE_DIVISA = 'MXP' \n"+
+						"ORDER BY FECHA DESC \n";
+			}else{
+				sSql =  "SELECT 0 SDO_EFECTIVO FROM DUAL";
+			}
 			
 			System.out.println("saldo de la cuenta individual¨****************************************"+sSql);
 			
 		}else if (parametros.getDefCampo("CONSULTA").equals("SALDO_GRUPAL")) {
 	
-			sSql =  "SELECT \n"+
+			sSql =  "SELECT * FROM PFIN_SALDO \n";
+			ejecutaSql();
+			if (rs.next()){
+				sSql =  "SELECT \n"+
 			      "PG.ID_PRESTAMO_GRUPO, \n"+
 			      "TO_CHAR(SUM(NVL(A.SDO_EFECTIVO,0)),'999,999,999.99') SALDO_CUENTA, \n"+
 			      "SUM(NVL(A.SDO_EFECTIVO,0)) SDO_EFECTIVO, \n"+
@@ -69,6 +77,10 @@ public class SimPrestamoSaldoCuentaDAO extends Conexion2 implements OperacionCon
 			      "AND A.ID_CUENTA = P.ID_CUENTA_REFERENCIA \n"+
 			      "AND A.CVE_DIVISA = 'MXP' \n"+
 			      "GROUP BY PG.ID_PRESTAMO_GRUPO \n";
+				System.out.println("tiene saldo");
+			}else{
+				sSql =  "SELECT 0 SDO_EFECTIVO FROM DUAL";
+			}
 			
 			System.out.println("saldo de la cuenta grupal¨****************************************"+sSql);
 			
